e352b15eadb46a834bd7834c9b34c229b94bdf07
Johnson Chen <chnejohnson@gmail.com>
2023-11-04 16:43:35 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+	plugins: [
+		vue(),
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
+	build: {
+		assetsDir: 'assets',
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.lib.ts'),
+			name: 'pinia-component',
+		},
+		outDir: 'dist',
+		rollupOptions: {
+			external: ['vue', 'pinia'],
+			output: {
+				dir: 'dist',
+				globals: {
+					vue: 'Vue',
+					pinia: 'pinia',
+				},
+			},
+		},
+	},
+})
